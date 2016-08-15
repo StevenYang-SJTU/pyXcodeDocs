0169143458805f4fb098c01ba09d2f249203bf0c
Freddy  Yu <cycleuser@cycleuser.org>
2016-08-15 10:15:57 +0800

    O'Reilly

--- getOreilly.py
+++ getOreilly.py
@@ -1,27 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  7 21:37:33 2016
+
+@author: cycleuser
+@email: cycleuser@cycleuser.org
+
+Copyright 2016 cycleuser
+
+"""
+#!/usr/bin/env python
+
+# coding=utf-8
+
+lang = "python"
+
+
+
 from pandas import DataFrame
 import requests
 
-#link ="http://www.oreilly.com/data/free/archive.html"
-#link="http://www.oreilly.com/programming/free/"
-#link="http://www.oreilly.com/web-platform/free/"
-#link="http://www.oreilly.com/iot/free/"
-link="http://www.oreilly.com/webops-perf/free/"
+link0 ="http://www.oreilly.com/data/free/archive.html"
+link1="http://www.oreilly.com/programming/free/"
+link2="http://www.oreilly.com/web-platform/free/"
+link3="http://www.oreilly.com/iot/free/"
+link4="http://www.oreilly.com/webops-perf/free/"
+link5="http://www.oreilly.com/business/free/"
 
-file = requests.get(link)
-f0 = open('source.txt','w')
-f0.write(file.text)
-f0.close
+link =["http://www.oreilly.com/business/free/","http://www.oreilly.com/data/free/archive.html","http://www.oreilly.com/programming/free/","http://www.oreilly.com/web-platform/free/","http://www.oreilly.com/iot/free/","http://www.oreilly.com/webops-perf/free/"]
+
+
+file=[]
+
+for i in link:
+    file.append(requests.get(i))
+
+
+filewriter = open('source.txt','w')
+
+for j in file:
+    filewriter.write(j.text)
+
+filewriter.close
 
 
 names = []
 links = []
 result = ""
-f1 = open('source.txt','r')
-filedata = f1.read()
-f1.close()
+
+
+newf = open('source.txt','r')
+filedata = newf.read()
+newf.close()
+
 newdata = filedata.replace('href="' ,'\n')
 tmpdata = newdata.replace('free/' ,'free/files/')
-newerdata = tmpdata.replace('.csp' ,'.mobi\n')
+newerdata = tmpdata.replace('.csp' ,'.mobi\n') #change mobi into pdf or epub if you need
+
 f = open('newer.txt','w')
 f.write(newerdata)
 f.close()
@@ -34,7 +68,7 @@ for j in range(0, len(lines)):
     templines.append(lines[j].strip())
  
 for i in range(0, len(templines)):
-    if ".mobi" in templines[i]:
+    if ".mobi" in templines[i]:                   #change mobi into pdf or epub if you need
         names.append((templines[i]))
         result += (templines[i]) + "    ,"
 
